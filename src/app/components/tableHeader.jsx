fa4d58f7573f40a06b06a551d7006554c425bbf6
gfR616 <616gfr@gmail.com>
2023-10-16 09:35:21 +0500

    arrows+

--- src/app/components/tableHeader.jsx
+++ src/app/components/tableHeader.jsx
@@ -10,6 +10,17 @@ const TableHeader = ({ onSort, selectedSort, columns }) => {
     }
   }
 
+  const renderSortArrow = (selectedSort, currentPath) => {
+    if (selectedSort.path === currentPath) {
+      if (selectedSort.order === 'asc') {
+        return <i className="bi bi-caret-down-fill"></i>
+      } else {
+        return <i className="bi bi-caret-up-fill"></i>
+      }
+    }
+    return null
+  }
+
   return (
     <thead>
       <tr>
@@ -19,16 +30,9 @@ const TableHeader = ({ onSort, selectedSort, columns }) => {
             onClick={columns[column].path ? () => handleSort(columns[column].path) : undefined}
             {...{ role: columns[column].path && 'button' }}
             scope="col">
-            {columns[column].name}
+            {columns[column].name} {renderSortArrow(selectedSort, columns[column].path)}
           </th>
         ))}
-        {/* <th onClick={() => handleSort('name')} scope="col">Имя</th>
-        <th scope="col">Качества</th>
-        <th onClick={() => handleSort('profession.name')} scope="col">Профессия</th>
-        <th onClick={() => handleSort('completedMeetings')} scope="col">Колличество встреч</th>
-        <th onClick={() => handleSort('rate')} scope="col">Оценка</th>
-        <th onClick={() => handleSort('bookmark')} scope="col">Избранное</th>
-        <th /> */}
       </tr>
     </thead>
   )
