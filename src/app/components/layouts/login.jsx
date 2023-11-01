1a8d0bb4833646f8e1a7cae865442dc22c82a5cf
gfR616 <616gfr@gmail.com>
2023-11-01 14:34:40 +0500

    добавляем логина страницу

--- src/app/components/layouts/login.jsx
+++ src/app/components/layouts/login.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return <h1>loginPage</h1>
+  const [data, setData] = useState({ email: '', password: '' })
+
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({
+      ...prevState,
+      [target.name]: target.value
+    }))
+  }
+  return (
+    <form action="">
+      <div>
+        <label htmlFor="email">Почта</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={data.email}
+          onChange={handleChange}
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="password">Пароль</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={data.password}
+          onChange={handleChange}
+        ></input>
+      </div>
+    </form>
+  )
 }
 
 export default Login
