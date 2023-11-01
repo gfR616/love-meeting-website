95e3120bb991a36ff911b092798428b1fb53741f
gfR616 <616gfr@gmail.com>
2023-11-01 16:48:37 +0500

    валидация

--- src/app/components/layouts/login.jsx
+++ src/app/components/layouts/login.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import TextField from '../textField'
 
 const Login = () => {
   const [data, setData] = useState({ email: '', password: '' })
+  const [, setErrors] = useState()
 
   const handleChange = ({ target }) => {
     setData((prevState) => ({
@@ -9,28 +11,44 @@ const Login = () => {
       [target.name]: target.value
     }))
   }
+  useEffect(() => {
+    validate()
+  }, [data])
+
+  const validate = () => {
+    const errors = {}
+    for (const fieldName in data) {
+      if (data[fieldName].trim() === '') {
+        errors[fieldName] = `${fieldName} обязательно для заполнения`
+      }
+    }
+    setErrors(errors)
+    return Object.keys(errors).length === 0
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const isValid = validate()
+    if (!isValid) return
+    console.log(data)
+  }
   return (
-    <form action="">
-      <div>
-        <label htmlFor="email">Почта</label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          value={data.email}
-          onChange={handleChange}
-        ></input>
-      </div>
-      <div>
-        <label htmlFor="password">Пароль</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={data.password}
-          onChange={handleChange}
-        ></input>
-      </div>
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Электронная почта"
+        type="text"
+        name="email"
+        value={data.email}
+        onChange={handleChange}
+      />
+      <TextField
+        label="Пароль"
+        type="password"
+        name="password"
+        value={data.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
     </form>
   )
 }
