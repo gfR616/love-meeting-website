0b1c1d8a2851c29c8e45949419dc25024bda844e
gfR616 <616gfr@gmail.com>
2023-12-25 18:40:04 +0500

    дз

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import api from '../../../api'
 import Qualities from '../../ui/qualities'
-import { useHistory } from 'react-router-dom'
+import UserCard from '../../ui/userCard'
+import QualitiesCard from '../../ui/qualitiesCard'
 
 const UserPage = ({ userId }) => {
-  const history = useHistory()
   const [user, setUser] = useState()
+
   useEffect(() => {
     api.users.getById(userId).then((data) => setUser(data))
   }, [])
@@ -15,13 +16,18 @@ const UserPage = ({ userId }) => {
   }
   if (user) {
     return (
-      <div>
-        <h1> {user.name}</h1>
-        <h2>Профессия: {user.profession.name}</h2>
-        <Qualities qualities={user.qualities} />
-        <p>completedMeetings: {user.completedMeetings}</p>
-        <h2>Rate: {user.rate}</h2>
-        <button onClick={handleClick}>Изменить</button>
+      <div className="container">
+        <div className="row gutters-sm">
+          <div className="col-md-4 mb-3">
+            <UserCard user={user} />
+            <QualitiesCard data={user.qualities} />
+            <h2>Профессия: {user.profession.name}</h2>
+            <Qualities qualities={user.qualities} />
+            <p>completedMeetings: {user.completedMeetings}</p>
+            <h2>Rate: {user.rate}</h2>
+            <button onClick={handleClick}>Изменить</button>
+          </div>
+        </div>
       </div>
     )
   } else {
---
+++ src/app/components/ui/qualitiesCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const QualitiesCard = () => {
+  return <div></div>
+}
+
+export default QualitiesCard
