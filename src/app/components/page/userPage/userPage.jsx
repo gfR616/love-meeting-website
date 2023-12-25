f4c4e02d87decabecdce962e9891b914ae40ee74
gfR616 <616gfr@gmail.com>
2023-12-25 20:17:08 +0500

    дз-3

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import api from '../../../api'
 import UserCard from '../../ui/userCard'
 import QualitiesCard from '../../ui/qualitiesCard'
-import MeetengsCard from '../../ui/meetingsCard'
+import MeetingsCard from '../../ui/meetingsCard'
 import Comments from '../../ui/comments'
 
 const UserPage = ({ userId }) => {
@@ -20,7 +20,7 @@ const UserPage = ({ userId }) => {
           <div className="col-md-4 mb-3">
             <UserCard user={user} />
             <QualitiesCard data={user.qualities} />
-            <MeetengsCard value={user.completedMeetings} />
+            <MeetingsCard value={user.completedMeetings} />
           </div>
           <div className="col-md-8">
             <Comments />
--- src/app/components/ui/meetingsCard.jsx
+++ src/app/components/ui/meetingsCard.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const MeetengsCard = () => {
-  return <div></div>
+const MeetingsCard = ({ value }) => {
+  return (
+    <div className="card mb-3">
+      <div className="card-body d-flex flex-column justify-content-center text-center">
+        <h5 className="card-title">
+          <span>Completed meetings</span>
+        </h5>
+
+        <h1 className="display-1">{value}</h1>
+      </div>
+    </div>
+  )
 }
 
-export default MeetengsCard
+MeetingsCard.propTypes = {
+  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+}
+export default MeetingsCard
