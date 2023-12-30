edba4cba9641bd89af188b095c0d2ec153f624d8
gfR616 <616gfr@gmail.com>
2023-12-30 17:57:01 +0500

    дз

--- src/app/App.js
+++ src/app/App.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
-
+import { ToastContainer } from 'react-toastify'
 import Login from './layouts/login'
 import Main from './layouts/main'
 import Users from './layouts/users'
 import NavBar from './components/ui/navBar'
+import { ProfessionProvider } from './hooks/useProfession'
+import { QualitiesProvider } from './hooks/useQualities'
 
 export default function App() {
   return (
     <>
       <NavBar />
-      <Switch>
-        <Route path="/users/:userId?/:edit?" component={Users} />
-        <Route path="/login/:type?" component={Login} />
-        <Route exact path="/" component={Main} />
-        <Redirect to="/" />
-      </Switch>
+      <QualitiesProvider>
+        <ProfessionProvider>
+          <Switch>
+            <Route path="/users/:userId?/:edit?" component={Users} />
+            <Route path="/login/:type?" component={Login} />
+            <Route exact path="/" component={Main} />
+            <Redirect to="/" />
+          </Switch>
+        </ProfessionProvider>
+      </QualitiesProvider>
+      <ToastContainer />
     </>
   )
 }
--- src/app/components/ui/qualities/qualitiesList.jsx
+++ src/app/components/ui/qualities/qualitiesList.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Quality from './quality'
+import useQualities from '../../../hooks/useQualities'
 
 const QualitiesList = ({ qualities }) => {
+  const { isLoading } = useQualities()
+  if (isLoading) return 'Loading...'
   return (
     <>
       {qualities &&
-        qualities.map((qual) => <Quality key={qual._id} {...qual} />)}
+        qualities.map((qual) => {
+          return <Quality key={qual._id} id={qual._id} />
+        })}
     </>
   )
 }
